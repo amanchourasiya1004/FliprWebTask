09ca97cee0e0ccf77a822b21c605dfa03f13b6b4
Aman Kumar Chourasiya <amanchourasiya1004@gmail.com>
2020-08-15 19:42:25 +0530

    Model made

--- stock/views.py
+++ stock/views.py
@@ -3,17 +3,18 @@ from stock.models import Stock, CompanyData
 import datetime
 import csv
 
-def scrapData(filename, name):
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count != 0 and row[1] != 'null':
-                split_date = row[0].split('-')
-                d = datetime.date(int(split_date[0]), int(split_date[1]), int(split_date[2]))
-                a = CompanyData(company_name = name, date = d, open_stock = float(row[1]), high = float(row[2]), low = float(row[3]), close = float(row[4]), adj_close = float(row[5]), volume = float(row[6]))
-                a.save()
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+
+# def scrapData(filename, name):
+#     with open(filename) as csv_file:
+#         csv_reader = csv.reader(csv_file, delimiter=',')
+#         line_count = 0
+#         for row in csv_reader:
+#             if line_count != 0 and row[1] != 'null':
+#                 split_date = row[0].split('-')
+#                 d = datetime.date(int(split_date[0]), int(split_date[1]), int(split_date[2]))
+#                 a = CompanyData(company_name = name, date = d, open_stock = float(row[1]), high = float(row[2]), low = float(row[3]), close = float(row[4]), adj_close = float(row[5]), volume = float(row[6]))
+#                 a.save()
+#             line_count += 1
+#     print(f'Processed {line_count} lines.')
         
-scrapData('/home/captain/Flipr Web Task/flipr/Data/TATASTEEL.NS.csv', 'Tata Steel Ltd.')
+# scrapData('path_to_file', 'Type of Index/Company')
